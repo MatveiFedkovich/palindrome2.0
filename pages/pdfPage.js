065e2a82e1dd133b44b976eb4ac0fcdb9b4711cb
MatveiFedkovich <m.fedkovich71@gmail.com>
2025-02-03 21:08:19 +0300

    fix problem with pdf page

--- pages/pdfPage.js
+++ pages/pdfPage.js
@@ -1,28 +1,18 @@
-// import '../styles/styles.css';
-import { useState, useEffect } from "react";
-import Navbar from '../components/Navbar';
+import dynamic from "next/dynamic";
+import Navbar from "../components/Navbar";
+
+const PdfViewer = dynamic(() => import("../components/PdfViewer"), { ssr: false });
 
 const PdfPage = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
   return (
     <div className="flex flex-col items-center justify-center text-center mt-14">
       <Navbar />
-      
+      <h1 className="text-3xl md:text-4xl font-bold mt-10">Читать PDF-документ</h1>
+      <p className="text-lg md:text-xl mt-4 max-w-2xl">
+        Ниже представлен PDF-документ, который вы можете пролистывать.
+      </p>
       <div className="mt-6 w-full max-w-4xl">
-        {isClient ? (
-            <iframe 
-                src="task.pdf"
-                className="w-full h-screen"
-                title="PDF Document"
-            ></iframe>
-        ) : (
-          <p>Загрузка...</p>
-        )}
+        <PdfViewer />
       </div>
     </div>
   );
